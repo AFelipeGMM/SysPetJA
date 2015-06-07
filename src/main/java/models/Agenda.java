8065623e254e82c1233de94fcc698f7bee46042c
norberto <norberto@Aspire-M5-481T>
2015-06-07 16:20:36 -0300

    detakhes para agenda

---
+++ src/main/java/models/Agenda.java
@@ -0,0 +1,55 @@
+package models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Agenda implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Temporal(TemporalType.TIME)
+	private Date horaInicio;
+	@Temporal(TemporalType.TIME)
+	private Date horaFim;
+	
+	private Animal animal;
+	private Servico servico;
+	private boolean Status = false;
+	
+	public Animal getAnimal() {
+		return animal;
+	}
+	public void setAnimal(Animal animal) {
+		this.animal = animal;
+	}
+	public Servico getServico() {
+		return servico;
+	}
+	public void setServico(Servico servico) {
+		this.servico = servico;
+	}
+	public boolean isStatus() {
+		return Status;
+	}
+	public void setStatus(boolean status) {
+		Status = status;
+	}
+	
+	
+
+}
