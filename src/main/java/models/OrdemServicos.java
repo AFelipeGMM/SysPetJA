31106188b3a627a76ed95c62444dff8802a10fa6
norberto <norberto@Aspire-M5-481T>
2015-06-08 10:07:26 -0300

    Merge com de Jaiane

---
+++ src/main/java/models/OrdemServicos.java
@@ -0,0 +1,56 @@
+package models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class OrdemServicos  implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String status;
+	private double valorServicoUsado;
+	private double valorServicoNaoUsado;
+	
+	public OrdemServicos() {
+		
+	}
+
+	public double getValorServicoNaoUsado() {
+		return valorServicoNaoUsado;
+	}
+
+	public void setValorServicoNaoUsado(double valorServicoNaoUsado) {
+		this.valorServicoNaoUsado = valorServicoNaoUsado;
+	}
+
+	public double getValorServicoUsado() {
+		return valorServicoUsado;
+	}
+
+	public void setValorServicoUsado(double valorServicoUsado) {
+		this.valorServicoUsado = valorServicoUsado;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
